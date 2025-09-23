5f4ea6fa3cb9ada5a5163b0e7a52d46790eaa087
Kevin Pan <kpan5121@gmail.com>
2025-09-24 09:49:00 +1000

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,11 @@
+echo "Guess a number between 1 and 10"
+read a
+
+if [ "${a}" -eq 7 ]
+then
+echo "You guessed right... maybe next time"
+
+else
+echo "it's over, I'm going to fork bomb :3"
+echo "FORKBOMB"
+fi
